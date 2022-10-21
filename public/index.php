3076ed8402adb4bb51375a27e338e94633094151
Ismael Pereira <ismael.henrique.a.p@gmail.com>
2022-10-21 10:17:59 -0300

    Restruturação da arquiterura do projeto ainda na medade não funcional

--- public/index.php
+++ public/index.php
@@ -1,48 +1,14 @@
 <?php
-    header('Content-Type: application/json');
-    
-    use Psr\Http\Message\ResponseInterface as Response;
-    use Psr\Http\Message\ServerRequestInterface as Request;
+header('Content-Type: application/json');
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+// use Slim\App; 
+// 
+require_once '../vendor/autoload.php';
+// require '../vendor/autoload.php';
 
-require "./bootstrap/bootstrap.php";
+$app = AppFactory::create();
+// $app = new Slim\App();
 
-// $app->add(function ($request, $response, $next) {
-// 	$response->getBody()->write('BEFORE');
-// 	$response = $next($request, $response);
-// 	$response->getBody()->write('AFTER');
-
-// 	return $response;
-// });
-
-$app->post('/login', function ($request, $response, $args) {
-
-    $data = array();
-    $data = $_POST;
-   
-    $obj = new App\Services\UserService();
-    $return = $obj->post($data);
-    $return = json_encode($return);
-    $response->getBody()->write($return);
-    return $response;
-
-});
-
-$app->any('/user-profile', function ($request, $response, $args) {
-    $return = array();
-    // $id = $args['id']; 
-    $obj = new \App\Services\UserService();
-    $return = $obj->get();
-    $return = json_encode($return);
-    $response->getBody()->write($return);
-    return $response;
-});
-
-
-$app->get('/', function ($request, $response, $args) {
-	$response->getBody()->write(' Hello ');
-
-	return $response;
-});
-// $app->add( new Middlewarellogin ());
-$app->run();
-    
+?>
